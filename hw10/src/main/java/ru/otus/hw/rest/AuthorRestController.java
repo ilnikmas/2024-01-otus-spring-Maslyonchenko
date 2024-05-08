20addbd70bc7f3b30764a76969a1ced4014c55f2
Ilya Maslenchenko <ilnikmas@tut.by>
2024-05-08 19:45:56 +0300

    #hw10 исправление замечаний

--- hw10/src/main/java/ru/otus/hw/rest/AuthorRestController.java
+++ hw10/src/main/java/ru/otus/hw/rest/AuthorRestController.java
@@ -1,7 +1,10 @@
 package ru.otus.hw.rest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.services.AuthorService;
 
